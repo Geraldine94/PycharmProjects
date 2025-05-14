793bdcc092b55e5b03017e262b30ad711abc2e36
Geraldine94 <geraldine_berenice@hotmail.com>
2025-05-14 19:26:58 -0300

    first commit

---
+++ value_assignment.py
@@ -0,0 +1,23 @@
+#valor numérico
+number = 10
+print(number)
+print(type(number))
+
+#valor alfanumérico
+text = "This is a letter"
+print (text)
+
+#valor booleano
+value = True
+
+#suma
+num1 = 7
+num2 = 2
+sum = num1 + num2 / 3
+print ("The result is" ,sum)
+
+#tipado dinámico
+variable = 24
+print (variable)
+variable = "Character"
+print (variable)
